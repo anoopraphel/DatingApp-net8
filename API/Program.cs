4a62158c5d6f38410c53852a1358c3b35b1b21d7
Anoop Raphel <anooparunpalliyan@gmail.com>
2025-06-03 22:58:50 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -5,12 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(Opt=>
+builder.Services.AddDbContext<DataContext>(Opt =>
 {
     Opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-}
-    
-);
+});
+
+builder.Services.AddCors();
 var app = builder.Build();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 app.Run();
