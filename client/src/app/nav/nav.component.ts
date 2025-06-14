6d2d60ad9f923c548bee1456a5169fdb0ce133df
Anoop Raphel <anooparunpalliyan@gmail.com>
2025-06-14 21:18:04 +0530

    End of section 6, add routing , new pages, style package install bootswatch, adding guard, add ngx-toaster for popup

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,17 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,BsDropdownModule,RouterLink,RouterLinkActive],
+  imports: [FormsModule,BsDropdownModule,RouterLink,RouterLinkActive,TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountservice=inject(AccountService)
   private router=inject(Router);
+  private toatr= inject(ToastrService)
   model:any={};
  login(){
   this.accountservice.login(this.model).subscribe(
@@ -21,7 +24,7 @@ export class NavComponent {
       next:response=>{
       this.router.navigateByUrl("/members")
     },
-    error:error=>console.log('error')
+    error:error=>this.toatr.error(error.error)
     }
   );
   }
