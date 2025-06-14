4e9c33d905e84eb64d04295af0e10dff7d2e4b06
Anoop Raphel <anooparunpalliyan@gmail.com>
2025-06-14 19:23:00 +0530

    Change associates with section 6 part 1

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,31 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,BsDropdownModule],
+  imports: [FormsModule,BsDropdownModule,RouterLink,RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountservice=inject(AccountService)
+  private router=inject(Router);
   model:any={};
  login(){
   this.accountservice.login(this.model).subscribe(
     {
       next:response=>{
-      console.log(response);
+      this.router.navigateByUrl("/members")
     },
-    error:error=>console.log(error)
+    error:error=>console.log('error')
     }
   );
   }
     logout(){
-    this.accountservice.logout();
+      this.accountservice.logout();
+    this.router.navigateByUrl("/")
   }
 }
 
